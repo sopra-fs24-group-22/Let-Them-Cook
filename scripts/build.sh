68ad22dff4a9132f2eb85c21a16190beaf743b1d
Gian Gyger <gian.gyger@ggsoftware.ch>
2024-03-21 13:11:55 +0100

    pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ rm -rf server/src/main/resources/static/*
 cp -r client/build/* server/src/main/resources/static/
 cd server && ./gradlew build && cd ../
 
-docker build -t let-them-cook:alpha .
+docker build -t let-them-cook:latest .

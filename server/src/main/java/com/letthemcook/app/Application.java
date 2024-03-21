4cdb7d4da55f65a0ca6dbdb4f5b52dad9eb7a70e
Gian Gyger <gian.gyger@ggsoftware.ch>
2024-03-21 10:41:17 +0100

    packaging

--- server/src/main/java/com/letthemcook/app/Application.java
+++ server/src/main/java/com/letthemcook/app/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,10 +18,17 @@ public class Application {
     SpringApplication.run(Application.class, args);
   }
 
+  // Route all requests to the static react app except for the /api route
+  @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}", "/error" })
+  public ModelAndView getIndex() {
+    return new ModelAndView("forward:/index.html");
+  }
+
+  // Return a simple message to confirm that the application is running
   @GetMapping(value = "/api", produces = MediaType.TEXT_PLAIN_VALUE)
   @ResponseStatus(HttpStatus.OK)
   @ResponseBody
-  public String helloWorld() {
+  public String appIsRunning() {
     return "The application is running.";
   }
 

1605270de5056bb6c426777e6cec4c536791110f
Claudio Fleischmann <c.fleischmann@picture-planet.ch>
2024-04-01 21:06:27 +0200

    New session single steps #40

---
+++ client/src/components/ui/Icon.tsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export function SecondaryIconButton({icon, style, onClick}: {icon: any, style: {}, onClick: () => void}) {
+  return <FontAwesomeIcon
+    icon={icon}
+    style={Object.assign({}, {color: '#878787'}, style)}
+    onClick={onClick}
+  />;
+}

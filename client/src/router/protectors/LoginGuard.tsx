9a3080dd604a6ba7dc4f6534daaa9a63e732f19a
David <davidmanuel.lanz@uzh.ch>
2024-04-11 11:45:59 +0200

    Updated AppGuard and LoginGuard #34

--- client/src/router/protectors/LoginGuard.tsx
+++ client/src/router/protectors/LoginGuard.tsx
@@ -2,13 +2,21 @@ import { Navigate, Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
 
 /**
+ * Checks if refresh token cookie exits and is valid
+ */
+const isAuthenticated = () => {
+  const refreshToken = document.cookie.split(';').find(c => c.trim().startsWith('refreshToken='));
+  return refreshToken ? true : false;
+};
+
+/**
  * Checks if user is not logged in
  * @Guard
  * @param props
  */
 export const LoginGuard = () => {
   // TODO: login check
-  if (true) {
+  if (!isAuthenticated()) {
     return <Outlet />;
   }
 

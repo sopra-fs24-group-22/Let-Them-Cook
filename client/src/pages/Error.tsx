90d7e25181f500e636a7b1ab97f82194112e5aff
Claudio Fleischmann <c.fleischmann@picture-planet.ch>
2024-04-26 17:21:18 +0200

    Error 404 page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,16 +1,23 @@
-import Layout from "../components/Layout/LoginLayout";
+import { useNavigate } from "react-router-dom";
+import Layout from "../components/Layout/ErrorLayout";
 import { Header1 } from "../components/ui/Header";
+import { Button } from "../components/ui/Login";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
-    <Layout>
+    <Layout errorCode="404">
       <Header1>Something's wrong I can feel it.</Header1>
       <img
         src="gordon-ramsay-no.gif"
         width="100%"
-				style={{margin: '20px 0 10px 0'}}
+				style={{margin: '20px 0'}}
         alt="Nope"
         />
+      <Button
+        onClick={() => navigate("/")}
+        style={{margin: '0'}}>
+        Back to the Dashboard</Button>
     </Layout>
   );
 };

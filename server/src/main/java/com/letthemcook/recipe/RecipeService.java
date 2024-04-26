8cc7af4ab13c65314301bc9a1396aabb8eb2f3df
Alzameister <tristkoning@gmail.com>
2024-04-26 11:50:16 +0200

    Fix private recipe get

--- server/src/main/java/com/letthemcook/recipe/RecipeService.java
+++ server/src/main/java/com/letthemcook/recipe/RecipeService.java
@@ -86,7 +86,7 @@ public class RecipeService {
 
     User user = userRepository.getByUsername(jwtService.extractUsername(accessToken));
 
-    if (recipe == null || (!Objects.equals(recipe.getCreatorId(), user.getId()) && recipe.getPrivacyStatus() == 0)) {
+    if (recipe == null) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe not found");
     }
     return recipe;

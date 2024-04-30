e31c753cdd9fff7b58c713ecfd5e29b938b0fb44
Claudio Fleischmann <c.fleischmann@picture-planet.ch>
2024-04-30 07:38:23 +0200

    Dashboard: Personalized title

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import Layout from "../components/Layout/MainLayout";
 import { Header1 } from "../components/ui/Header";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faKitchenSet } from "@fortawesome/free-solid-svg-icons";
+import { getMyUser } from "../api/user.api";
 
 const AppPage = () => {
+  const ERROR_LOADING_DASHBOARD = "Error while loading the dashboard. Please reload the page.";
+  useEffect(() => {
+    fetchUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // User
+  const [user, setUser] = useState<any>(null);
+  const fetchUser = async () => {
+    try {
+      const user = await getMyUser();
+      console.log(user);
+      setUser(user);
+    } catch (e) {
+      alert(ERROR_LOADING_DASHBOARD);
+    }
+  };
+
+  // --- RETURN ---
   return (
     <Layout>
       <Header1>
-        Let them Cook <FontAwesomeIcon icon={faKitchenSet} />
+        Let {(user?.firstname) ? user.firstname : 'them'} Cook!
       </Header1>
     </Layout>
   );
